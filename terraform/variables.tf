1d0b5399c3317248160f9437f975e68ba8a82eae
Andrey V. Romanchev <andrey.romanchev@gmail.com>
2018-07-02 23:58:21 +0300

    Task terraform-1, mandatory

---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "project" {
+  description = "Project ID"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "europe-north1"
+}
+
+variable "public_key_path" {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key used for ssh access"
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "disk_image" {
+  description = "Disk image"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = "europe-north1-c"
+}
